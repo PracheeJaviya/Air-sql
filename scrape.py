0d71ac01d493e1769b718379ba89dd09cae9f0d2
root <root@localhost.localdomain>
2020-03-24 17:50:12 +0530

    WubbaLubbaDubDub

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ import csv
 url_airindia = requests.get('http://www.airindia.in/time-table.htm').text
 
 from bs4 import BeautifulSoup
-f = open("bufftry.txt", "w")
+# f = open("bufftry.txt", "w")
 soup = BeautifulSoup(url_airindia, 'lxml')
 
 
@@ -17,11 +17,11 @@ for tr in tr_data:
     row = [i.text for i in td_data]
     td_data_final.append(row)
 
-print(td_data_final)
-f.write(str(td_data_final))
-f.close()
-#td_data_final = [sub.replace('\xa0', '0') for sub in td_data_final] 
+for row in td_data_final:
+    for s_data in row:
+        s_data = [el.replace('\xa0','0') for el in s_data]
 
-#with open('data.csv', 'w') as file:
- #   writer = csv.writer(file, delimiter='|')
-  #  writer.writerows(td_data_final)
+#
+with open('data.csv', 'w') as file:
+   writer = csv.writer(file, delimiter='|')
+   writer.writerows(td_data_final)
