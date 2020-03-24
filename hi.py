9290115eddd74da790e98d2b9c91622558e728c6
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-03-24 16:18:14 +0530

    'scrapped'

--- hi.py
+++ hi.py
@@ -1,12 +1,24 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+# import requests
+
+# try:
+#     f = open("bufftry.txt", "w")
+#     page = requests.get('http://www.airindia.in/time-table.htm')
+#     soup = bs4.BeautifulSoup(page.content, 'lxml')
+#     contents = soup.find("div", class_="tableData")
+#     f.write(contents)
+# except Exception:
+#     f.write("Prachee hiii")
+
+# f.close()
+
 import requests
+from bs4 import BeautifulSoup
+f = open("bufftry.txt", "w")
+url = 'http://www.airindia.in/time-table.htm'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "lxml")
 
-
-def scrap() -> None:
-    f = open("bufftry.txt", "w")
-
-    page = requests.get('http://www.airindia.in/time-table.htm')
-    soup = bs4.BeautifulSoup(page.content, 'lxml')
-    contents = soup.find(id='ContentPlaceHolder1_TimeTableControl1_gvTimeline')
-    f.write(contents)
-    f.close
+b = soup.findAll(class_="tr3")
+f.write(str(b))
+f.close()
