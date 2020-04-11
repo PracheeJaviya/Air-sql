27a5cebc3532cf45c603445dfa6208dcb1e741bd
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-04-11 18:25:48 +0530

    fetching data

--- connect.py
+++ connect.py
@@ -13,20 +13,27 @@ def connect():
         # create a cursor
         cur = conn.cursor()
 
-   # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
+        sel_from_dest = " select * from flight where fcity = 'Delhi' "
 
+        # execute a statement
+        # print('PostgreSQL database version:')
+        cur.execute(sel_from_dest)
+
+        records =cur.fetchall()
+        print("DATASET")
+        for row in records :
+            print( "From " , 'Delhi')
+            print(" To ", row[1])
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
 
-        cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
+            cur.close()
             print('Database connection closed.')
 
 
