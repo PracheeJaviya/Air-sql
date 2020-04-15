c8e3dd02887025cd3bd77dae92cdfc40455d0437
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-04-15 15:29:41 +0530

    fare

--- Procedures/random.sql
+++ Procedures/random.sql
@@ -7,9 +7,8 @@ price integer;
 begin
  select count(freq) into total from flightdetails;
  for i in 1..total loop
-    price = select floor(random() * 7000 + 5000)::int;
-    update flightdetails set fare=price where index=i;
+    -- price[i] = select floor(random() * 7000) + 5000::int;
+    update flightdetails set fare=(select floor(random() * 7000) + 5000::int) where index=i;
  end loop;
-
 end;
 $$language plpgsql;
