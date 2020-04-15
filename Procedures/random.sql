3614437bc66e46254a7e456479ae497ebab02a75
Prachee Javiya <prachee.j@ahduni.edu.in>
2020-04-16 01:25:08 +0530

    fare w cursor

--- Procedures/random.sql
+++ Procedures/random.sql
@@ -2,13 +2,20 @@
 create function fare() returns void as
 $$
 declare
+airname text;
+aircur cursor for select aircraft from fare;
 total integer;
-price integer;
+-- price integer;
 begin
- select count(freq) into total from flightdetails;
+open cursor;
+
+ select count(aircraft) into total from fare;
  for i in 1..total loop
+ fetch aircur into airname;
     -- price[i] = select floor(random() * 7000) + 5000::int;
-    update flightdetails set fare=(select floor(random() * 7000) + 5000::int) where index=i;
+    update fare set fare=(select floor(random() * 1000) + 5000::int);
+    update fare set bfare=(select floor(random()*13000)+40000::int);
+exit when not found;
  end loop;
 end;
 $$language plpgsql;
